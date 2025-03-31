ff2e5efad26a482d41c1f3b3802fffa21350ed1e
JCGreen03 <160683432+JCGreen03@users.noreply.github.com>
2025-03-30 20:25:58 -0400

    Add files via upload

---
+++ Week-1/Foundation/About.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="CSS/about.css">
+</head>
+
+<body>
+
+
+    <?php
+    Include "./Components/Navbar.php";
+    ?>
+
+    <div class="container">
+        <p>Welcome to our organization. We are dedicated to providing services in a christlike manner.
+        For years, we have provided quality communication, products, and other services. We are a God-fearing, American made, and
+        family focused organization dedicated to serving others and, above all, providing quality service in the name of
+        our Lord Jesus Christ.
+        </p>
+
+        <hr />
+
+        <p>Our founder was Guy Mann. Guy was lost in life, but when he found the Lord during his late teens,
+            he quickly found his calling in serving others in the name of God. He quickly formed this organization
+            in order to further his mission of providing service and doing so in a Godly way. For the past
+            years, we have reached over a dozen communities with our projects and provided products to hundreds more.
+        </p>
+    </div>
+
+    <?php
+    Include "./Components/Footer.php";
+    ?>
+</body>
+
+</html>
---
+++ Week-1/Foundation/Components/Navbar.php
@@ -0,0 +1,10 @@
+<?php
+    
+    echo '<ul class="navbar">
+        <li><a href="/HomeMenu.php">Back to Menu</a></li>
+        <li><a href="FoundationsHome.php">Home</a></li>
+        <li><a href="About.php">About us</a></li>
+        <li><a href="PHPconfig.php">Configuration</a></li>
+        <li><a href="Contact.php">Contact Us</a></li>
+          </ul>';
+    ?>
